e1af8f0e35b9aea1d1e1422b428c7fa8a0a5d468
Nathan Nette <nathan.nette@gmail.com>
2020-07-11 22:43:45 +1000

    Created basic blockout and vehicle script
    The car can currently just go forwards but the very basics have been established.

---
+++ UnityFiles/SlotCars/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    public float fSpeed = 0f;
+
+    private Rigidbody rbVehicle = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbVehicle = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            InputFunc();
+        }
+    }
+
+    void InputFunc()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            rbVehicle.AddForce(gameObject.transform.forward * fSpeed);
+        }
+    }
+}
